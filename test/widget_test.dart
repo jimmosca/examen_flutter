26f0ae9f4d10485ab1bbb5e0b1d225be0c3e4f96
jaime.casado <jaime.casado.a@gmail.com>
2020-02-14 10:19:50 +0100

    Configuracion inicial de proyecto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:examen_flutter_casadojaime/main.dart';
+import 'package:examen_flutter_casadojaime/ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
