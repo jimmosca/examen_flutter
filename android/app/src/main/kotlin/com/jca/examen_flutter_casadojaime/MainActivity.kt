72268f78b8dcdb660150d790cdcf5c72125c1885
jaime.casado <jaime.casado.a@gmail.com>
2020-02-14 10:08:43 +0100

    root commit

---
+++ android/app/src/main/kotlin/com/jca/examen_flutter_casadojaime/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jca.examen_flutter_casadojaime
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
